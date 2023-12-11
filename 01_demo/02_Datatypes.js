5a2f5c4970f321b1de6d0853300a4e2ce406476c
bhatt-jaymin <bhatt.jaymin@gmail.com>
2023-12-11 14:40:03 +0000

    Datatypes

---
+++ 01_demo/02_Datatypes.js
@@ -0,0 +1,23 @@
+"user strict"; // treat all js code as newer version
+
+//alert (3+3) // We are using node js, not browser 
+
+let name="DevTeam"
+let age=25
+let loggedIn=false
+let userState
+let cityTemparature=null
+
+//Datatypes
+
+// number => range 2 power 53
+// BigInt => not much use [mostly used in Large volume website,sharemarket,stock exchange,currency Market]
+// string => prefered use ""
+// boolean => true/false
+// null => standalone value
+// undefined => not define value;
+// symbol => use in react unique 
+
+// object
+
+console.table([typeof name,typeof age,typeof loggedIn, typeof userState, typeof cityTemparature]);
