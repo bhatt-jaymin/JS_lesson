bb94aa37f34bda5b89d8496b53e3706ac4f2d83a
bhatt-jaymin <bhatt.jaymin@gmail.com>
2023-12-11 14:40:17 +0000

    variables

---
+++ 01_demo/01_Variables.js
@@ -0,0 +1,18 @@
+const userId='093467';
+let userEmail='devtest@yopmail.com'
+var userPassword='123456'; 
+accountCity='Surat';
+let accountState;
+
+// Not use var because issue in 
+//function scope and block scope.
+
+
+//userId=244; Not Allowed here
+userEmail='dtest@gl.com';
+userPassword='Admin_1234';
+accountCity='Ahemdabad';
+
+console.log(userId);
+
+console.table([userId,userEmail,userPassword,accountState,accountCity]); // display variable in tabular form
