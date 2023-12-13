b652a2d834b8d13ce3b490075de873e9825fe48e
bhatt-jaymin <bhatt.jaymin@gmail.com>
2023-12-13 03:46:45 +0000

    Conversion of Datatypes

---
+++ 01_demo/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score ="35abc"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let scoreValueInNumber=Number (score)
+
+console.log(typeof(scoreValueInNumber))
+
+console.log(scoreValueInNumber)
+
+let isLoggedIn= true
+let booleanIsLoggedIn=Boolean (isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+//"33" ==> 33
+//"33abc" ==> Nan
+//"null" ==> 0
+// boolean 1 value ==> true
+// boolean 0 value ==> false
+// boolean " " value ==> false
+// boolean "abc" value ==> true
