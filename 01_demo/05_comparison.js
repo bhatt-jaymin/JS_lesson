beaa901d015515f0684cab494641bd7746719724
bhatt-jaymin <bhatt.jaymin@gmail.com>
2023-12-14 13:40:17 +0000

    comparison operator

--- 01_demo/05_comparison.js
+++ 01_demo/05_comparison.js
@@ -1,7 +1,25 @@
-console.log(2>1)
-console.log(1>1)
-console.log(1>=1)
-console.log(1<=1)
-console.log(0<1)
-console.log(0<0)
-console.log(1<=3)
+/* console.log(2>1)   ==> True
+console.log(1>1)   ==> False  
+console.log(1>=1)  ==> True
+console.log(1<=1)  ==> True
+console.log(0<1)   ==> True
+console.log(0<0)   ==> false
+console.log(1<=3)  ==> True */
+
+/********************* 
+console.log("2">1) ==> True
+console.log("02">1)==> True */
+
+/***************** 
+console.log(null>0)  ==> false
+console.log(null<0)  ==> false
+console.log(null==0) ==> false
+console.log(null>=0) ==> True
+console.log(null<=0) ==> True 
+*/
+/* console.log(undefined>0)   ==> false
+console.log(undefined<0)   ==> false
+console.log(undefined==0)  ==> false
+console.log(undefined>=0)  ==> false
+console.log(undefined<=0)  ==> false */
+
