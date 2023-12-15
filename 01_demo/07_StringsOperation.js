6d4a1b663af2d1f261b91001b02d4173f227eb33
bhatt-jaymin <bhatt.jaymin@gmail.com>
2023-12-15 00:43:15 +0000

    String & Datatypes Function Oprtation

---
+++ 01_demo/07_StringsOperation.js
@@ -0,0 +1,23 @@
+const name="Jaimin"
+const repoCount=80
+
+// console.log(`${name} have more than ${repoCount} repo in git and other repo tool in different Accounts `) //String interpolation
+
+// Declare String variable other way
+const myAliasName= new String("Jay") // display as object in browser console
+
+const myFirstAliasName= new String("Hello JS").substring(1,5)
+
+const mySecondAliasName= myFirstAliasName.slice(-8,3)
+
+/* console.log(myAliasName[1])
+console.log(myAliasName.__proto__)// syntax of String prototype */
+
+console.log(myAliasName.length)
+console.log(myAliasName.toUpperCase())
+console.log(myAliasName.charAt(1))
+console.log(myAliasName.indexOf('y'))
+
+console.log(myFirstAliasName)
+console.log(mySecondAliasName)
+
