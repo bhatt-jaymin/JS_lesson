d7f990788e94f9c34632e6106417aaef83b0f815
jaymin1088 <jaymin_10_88@yahoo.co.in>
2023-12-26 19:55:40 +0530

    Update 8_NumandMaths.js
    All Number and Maths inbuilt function  update successfully

--- 01_demo/8_NumandMaths.js
+++ 01_demo/8_NumandMaths.js
@@ -1,1 +1,41 @@
+let no=new Number(23466768787)
+
+let count=new Number(234.50768787)
+
+let thousand=new Number(200000000)
 //console.log("2"+"2"-"2") ===> 20
+
+//console.log(2 + 9) ===> 11
+
+//console.log(no) ===> [Number: 23466768787]
+
+// To String convert number type into String
+
+// console.log(no.toFixed(2)) ===> 23466768787.00
+
+
+
+/*
+
+// toPrecision number function
+
+let count=new Number(234.42768787)
+console.log(count.toPrecision(4)) ===> 234.4
+
+let count=new Number(234.45768787)
+console.log(count.toPrecision(4)) ===> 234.5
+
+let count=new Number(234.44768787)
+console.log(count.toPrecision(3)) ===> 234
+
+let count=new Number(234.50768787)
+console.log(count.toPrecision(3)) ===> 235
+
+*/
+
+// console.log(thousand.toLocaleString()) ====> 200,000,000
+
+
+// =================== MATHS ==========================
+
+
