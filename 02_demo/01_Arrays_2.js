43d69fce72e1734cd3dd6c9f1200260fbe2059bb
jaymin1088 <jaymin_10_88@yahoo.co.in>
2023-12-27 17:58:52 +0530

    Update 01_Arrays_2.js
    Adding new Array Function

--- 02_demo/01_Arrays_2.js
+++ 02_demo/01_Arrays_2.js
@@ -11,12 +11,29 @@ console.log(myFirstArray); // [ 0, 3, 5, 7, 9, [ 0, 13, 15, 17, 19 ] ]
 
 console.log(myFirstArray.concat(mySecondArray));
 
+console.log([...myFirstArray,...mySecondArray]); // Spread method concate multiple array other cocate method
+
+above 2 spread and cocate methods o/p display same
 [
   0,  3,  5,  7,  9,
   0, 13, 15, 17, 19
 ]
 
+const myAnotherArray=[2,4,7,[9,7,5],8,[2,7]];
+const myNewAnotherArray=myAnotherArray.flat(Infinity);
+console.log(myNewAnotherArray); // create new array in single array
+
+[
+  2, 4, 7, 9, 7,
+  5, 8, 2, 7
+]
+
+console.log(Array.isArray("Jaimin"));===> False // check array available or not
+
+console.log(Array.from("Jaimin"));===> [ 'J', 'a', 'i', 'm', 'i', 'n' ] //Adding new array
+
+console.log(Array.from({name:"jaimin"}));[]  // Interesting Array is not define  
 */
-console.log([]);
 
 
+console.log(Array.from({name:"jaimin"}));
