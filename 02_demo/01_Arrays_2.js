142e6199991885c9d6c5d8d360fc27c88b60b980
jaymin1088 <jaymin_10_88@yahoo.co.in>
2023-12-27 17:45:16 +0530

    Advance Basics
    Adding new Array Methods

---
+++ 02_demo/01_Arrays_2.js
@@ -0,0 +1,22 @@
+// Array Detail concept
+
+let myFirstArray=[0,3,5,7,9]
+let mySecondArray=[0,13,15,17,19]
+
+/* concate first array to Second array
+
+myFirstArray.push(mySecondArray)
+console.log(myFirstArray); // [ 0, 3, 5, 7, 9, [ 0, 13, 15, 17, 19 ] ]
+
+
+console.log(myFirstArray.concat(mySecondArray));
+
+[
+  0,  3,  5,  7,  9,
+  0, 13, 15, 17, 19
+]
+
+*/
+console.log([]);
+
+
