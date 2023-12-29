e493179ddcc4a9025dc8532514e5b9b06a82feaa
jaymin1088 <jaymin_10_88@yahoo.co.in>
2023-12-29 10:29:35 +0530

    Function Diffrent Methods
    Adding new Function and Its methods also learn function calling,parameters,arguments of functions

--- 03_demo/01_Functions_1.js
+++ 03_demo/01_Functions_1.js
@@ -1,1 +1,101 @@
 // Functions
+
+/*
+function myFirstFunction(){ // Create function
+    console.log("A");
+    console.log("E");
+    console.log("I");
+    console.log("O");
+    console.log("U");
+}
+
+myFirstFunction() //calling function
+
+O/P
+
+A
+E
+I
+O
+U
+
+*/
+
+/*Method1
+
+function myAddition(num1,num2){
+
+    console.log(num1+num2); 
+}
+
+const result= myAddition(10,20)
+
+30
+*/
+
+/*Method2
+
+function myAddition(num1,num2){
+
+    let result=num1+num2; 
+    return result
+}
+
+const result= myAddition(10,20)
+console.log(`Result : ${result}`);
+
+30
+*/
+
+/*Method3
+
+function myAddition(num1,num2){
+
+    return  num1+num2
+}
+
+const result= myAddition(10,20)
+console.log(`Result : ${result}`);
+
+30
+*/
+
+/*Method4
+
+function loginMessage(userLoggedIn){
+
+    return  `${userLoggedIn} logged In`
+}
+
+console.log(loginMessage("Jaimin"));
+
+Jaimin logged In
+
+*/
+
+/*Method4
+
+function loginMessage(userLoggedIn){
+    if(!userLoggedIn){
+        console.log("Please enter valid Username");
+        return
+    }
+
+    return  `${userLoggedIn} logged In`
+}
+
+console.log(loginMessage(""));
+
+Please enter valid Username
+
+*/
+function loginMessage(userLoggedIn = "Guest") {
+  if (!userLoggedIn) {
+    console.log("Please enter valid Username");
+    return
+  }
+
+  return `${userLoggedIn} User logged In`;
+}
+
+console.log(loginMessage());
