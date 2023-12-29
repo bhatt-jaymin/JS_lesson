0a595591a0cbe5cde60a7c1d26d2ad857c273cad
jaymin1088 <jaymin_10_88@yahoo.co.in>
2023-12-29 15:22:01 +0530

    Update Advance scope concept
    Update Advance scope concept with array and object

--- 03_demo/04_Advance_Scopes.js
+++ 03_demo/04_Advance_Scopes.js
@@ -1,1 +1,38 @@
 //Scope levels and mining
+
+/* function one(){
+
+  const userName="Jaimin"
+  function two(){
+    const website="youtube.com"
+    console.log(userName);
+  }
+  //console.log(website);
+
+ // two()
+}
+
+//one() */
+
+/* if(true){
+  const user="Jaimin";
+
+  if(user===" Jaimin "){
+  const URL="youtube";
+  console.log(user + URL);
+}
+//console.log(URL);
+} */
+
+function addOne(num){
+  return num+1
+}
+
+addOne(5) // 6
+
+
+addTwo() // error display
+const addTwo=function(num){
+  return num+2
+}
+
