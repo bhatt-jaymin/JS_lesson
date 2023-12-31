4ad9f29d057b294823ba8051d54dcedefde99d62
jaymin1088 <jaymin_10_88@yahoo.co.in>
2023-12-31 07:11:15 +0530

    Flow Control Statements
    Example of if else and Switch Statement

---
+++ 04_Demo/01_FlowControl.js
@@ -0,0 +1,46 @@
+// If Statement
+/* 
+let temprature = 41
+
+if (temprature === 41){
+    console.log(`Temprature is less than 51 `)
+}
+else{
+    console.log(`Temprature is greater than 51`);
+}
+console.log(`Executed`); */
+
+// <,>,<=,>=,==,===,!=,!==
+
+
+/* const score =200
+
+if(score>100){
+    const power="fly"
+    console.log(`User power:- ${power}`);
+} */
+
+
+/* const balance=1000
+if(balance < 500 ){
+    console.log(`Less than 500`);
+}else if(balance<750){
+    console.log(`Less than 750`);
+}else if(balance<900){
+    console.log(`Less than 900`);
+}else{
+    console.log(`Less than 1200`);
+} */
+
+const allowLoggedIn=true
+const debitCard=true
+const allowLoggedInGoogle=false
+const allowLoggedInEmail=true
+
+if (allowLoggedInGoogle || allowLoggedInEmail) {
+    console.log(`User able to Log In Successfully`)
+}
+
+/* if (allowLoggedIn && debitCard) {
+    console.log(`User able to Log In`);
+} */
---
+++ 04_Demo/01_switchControl.js
@@ -0,0 +1,34 @@
+// Switch Statement
+
+/* switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+} */
+
+const switchMonth=3
+
+switch (switchMonth) {
+    case 1:
+        console.log(`January`);
+        break;
+
+    case 2:
+        console.log(`February`);
+        break;
+
+    case 3:
+        console.log(`March`);
+        break;
+        
+    case 3:
+        console.log(`April`);
+        break;
+
+    default:
+        console.log(`Default case Match`);
+        break;
+}
