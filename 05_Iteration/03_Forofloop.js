dc742148701ae309c0595574af3d8a7aec1c5597
jaymin1088 <jaymin_10_88@yahoo.co.in>
2024-01-02 01:36:00 +0530

    For Of and For In
    Explanation of For Of and For In

---
+++ 05_Iteration/03_Forofloop.js
@@ -0,0 +1,15 @@
+// for of loop
+
+/* const arr=[1,2,3,4,5]
+
+for (const numOfArry of arr) {
+    console.log(numOfArry);
+    
+} */
+
+const Msg="Hello World"
+
+for (const Message of Msg) {
+    console.log(`Each Character seprate of ${Message}`);
+    
+}
