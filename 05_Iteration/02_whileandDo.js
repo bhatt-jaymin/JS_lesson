de8c15696fbc8d1ca685dcf30d30d30c2487da22
jaymin1088 <jaymin_10_88@yahoo.co.in>
2024-01-02 01:23:36 +0530

    While and Do While Loop
    While and Do while Loop Concept

---
+++ 05_Iteration/02_whileandDo.js
@@ -0,0 +1,35 @@
+/* While Loop
+
+while (condition) {
+    
+} */
+
+/* let index=0
+while (index<=20) {
+console.log(`Index value is ${index}`);
+index=index+3
+    
+} */
+
+let myIdxArry=['jerry','Tom','Duckie']
+
+let idx=0
+while (idx< myIdxArry.length) {
+console.log(`Index value is ${myIdxArry[idx]}`);
+idx=idx+1
+    
+}
+
+/*
+
+Do While 
+do {
+    
+} while (condition);*/
+
+let score=1
+do {
+    console.log(`Score :- ${score}`);
+score++
+    
+} while (score<=20);
