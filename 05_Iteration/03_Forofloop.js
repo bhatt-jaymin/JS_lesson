538d2f70f17c1d6c07b6fd710d3df64a9d4378ea
jaymin1088 <jaymin_10_88@yahoo.co.in>
2024-01-02 09:54:27 +0530

    For Of Loop
    Example of For Of Loop

--- 05_Iteration/03_Forofloop.js
+++ 05_Iteration/03_Forofloop.js
@@ -7,9 +7,45 @@ for (const numOfArry of arr) {
     
 } */
 
-const Msg="Hello World"
+// For of for String
+
+/* const Msg="Hello World"
 
 for (const Message of Msg) {
     console.log(`Each Character seprate of ${Message}`);
     
+} */
+
+// MAP Key  ==> used for redundant duplicate value and display in fifo order
+
+/*
+
+1)
+
+const map=new Map()
+map.set('IN','Bharat')
+map.set('US','United States of America')
+map.set('UK','United Kingdom')
+
+//console.log(map);
+
+// key and value de structure using for loop in map key
+
+for (const [key,value] of map) {
+    console.log(key," :- ",value );
 }
+*/
+
+// for of loop in obhect using map key
+
+/* const myNewGameObj={ for information
+    game1:'IGI',
+    game2:'Mario',
+    game3:'Need For Speed',
+    game4:'GTA',
+    game5:'Counter Strike'
+}
+
+for (const [key,value] of myNewGameObj) {
+ console.log(key, ':-' , value);   
+} */
