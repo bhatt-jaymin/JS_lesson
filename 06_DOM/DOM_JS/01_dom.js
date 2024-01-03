f1f42b36635698b072d9447dc458ca3cfd5a8f72
jaymin1088 <jaymin_10_88@yahoo.co.in>
2024-01-03 13:19:17 +0530

    DOM Manipulation
    DOM Manipulation Methods with diffrent example

--- 06_DOM/DOM_JS/01_dom.js
+++ 06_DOM/DOM_JS/01_dom.js
@@ -1,2 +1,40 @@
 // Data Object Model [DOM]
 
+/*
+
+title.textContent  ===> display hidden and visible both text
+
+
+title.innerHTML ===> display hidden tag and element with text
+
+
+title.innerText ===> display visble text only.
+
+title.style.padding=1.5rem ===> set any CSS style in js file
+
+
+document.getElementById('title').setAttribute('class','test') ==> set class name
+
+document.getElementById('title').getAttribute('class') ===> get class element
+
+document.getElementById('title') ===> get page id element
+
+document.querySelector('h2') ==> first any element tag display of the page
+
+document.querySelectorAll('h2') ==> All  element tag display in page
+
+document.querySelector('input[type="password"]') any input element display in html page
+
+*/
+
+
+/* query selector
+
+const newLiTemp=document.querySelectorAll('li')
+
+newLiTemp[2].style.backgroundColor='green'
+
+newLiTemp.forEach(function (li){
+    li.style.backgroundColor='wheat'
+})
+*/
