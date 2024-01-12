90dbbee30d7dc45e7b198b663baf04952fcf0da2
jaymin1088 <jaymin_10_88@yahoo.co.in>
2024-01-12 12:32:52 +0530

    asynchronization
    example of asynchronization

---
+++ 08_Asynchronization/1_ascy.js
@@ -0,0 +1,27 @@
+/*
+JS asynchronization
+JS single thread
+
+Execution context
+
+Heap
+call Stack
+Memory Heap
+
+
+Blocking Code
+
+====> Block flow of the program
+====> Read file Synchronization
+
+
+
+
+None Blocking Code
+
+====> Do not Block flow of the execution
+====> Read file ASynchronization
+
+
+
+*/
