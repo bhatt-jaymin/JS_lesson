9af5ccc54b0c6c134caa191151ff88f50f68125d
jaymin1088 <jaymin_10_88@yahoo.co.in>
2024-01-15 18:58:49 +0530

    Lexical Scope
    explain Lexical Scope

---
+++ 13_LexicalandCloser/lexicalandcloser1.js
@@ -0,0 +1,18 @@
+// Lexical  Scope
+
+/* function Outer() {
+    let userName="jai"
+    function Inner() {
+        console.log(userName); // lexical Scope
+    }
+    Inner()
+}
+Outer()
+//console.log(userName); */
+
+
+// ===> not share with inner other function
+// ====> not share with parent function
+
+//Closer Scope
+
