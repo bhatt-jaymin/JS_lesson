b2e36b94473548d6a6a0e04915ae767906a0d25c
jaymin1088 <jaymin_10_88@yahoo.co.in>
2024-01-15 16:50:51 +0530

    Getter and Setter
    Getter and Setter in JS

--- 12_GettersandSetters/GettersandSetters1.js
+++ 12_GettersandSetters/GettersandSetters1.js
@@ -3,6 +3,15 @@ class User{
         this.email=email;
         this.password=password;
     }
+
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(values){
+        this._email=values.toUpperCase()
+    }
+
+
     get password(){
         return this._password.toUpperCase()
     }
@@ -13,3 +22,4 @@ class User{
 
 const jai=new User('adc@yopmail.com','abcxyz')
 console.log(jai.password);
+console.log(jai.email);
